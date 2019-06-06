33354502a6bc7052ef9139d6bb28d2531b536335
ffy4839 <ffy4839@163.com>
2019-06-06 17:10:42 +0800

    over

--- proxyPool/proxyPool.py
+++ proxyPool/proxyPool.py
@@ -29,13 +29,20 @@ class main():
                 return True
 
     def del_removel(self, data):
-        data = data.strip('\n').split('\n')
-        func = lambda x:int(x.split(';')[0])
-        data = list(set(data.sort(data,key=func,reverse=True)))
+        # str2list
+        if isinstance(data, str):
+            data = data.strip('\n').split('\n')
+        # 根据nums排序
+        func = lambda x: int(x.split(';')[0])
+        data.sort(key=func, reverse=True)
+        # 去重
+        data = list(set(data))
         data_ip = [i.split(';')[1] for i in data]
-        # data_nums = [i.split(';')[0] for i in data]
         nums = [data_ip.index(i) for i in list(set(data_ip))]
-        return [data[i] for i in nums]
+        data = [data[i] for i in nums]
+        # 根据nums排序
+        data.sort(key=func, reverse=True)
+        return data
 
 
 
